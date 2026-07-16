4fd851640d8a2f13d610053e0beebf9a8be5722a
peppenob <peppe190386@gmail.com>
2026-07-16 20:48:53 +0200

    Aggiorna e centralizza Tracking ID Amazon

--- lib/affiliate-links.ts
+++ lib/affiliate-links.ts
@@ -1,13 +1,11 @@
+export const AMAZON_TAG = "thekinglege05-21";
+
 export const affiliateLinks = {
-  prime:
-    "https://www.amazon.it/provaprime?tag=thekinglegend-21",
+  prime: `https://www.amazon.it/provaprime?tag=${AMAZON_TAG}`,
 
-  kindleUnlimited:
-    "https://www.amazon.it/kindle-dbs/hz/signup?tag=thekinglegend-21",
+  kindleUnlimited: `https://www.amazon.it/kindle-dbs/hz/signup?tag=${AMAZON_TAG}`,
 
-  audible:
-    "https://www.amazon.it/dp/B075CPFY5Y?actionCode=AZIOther35606092201BR&tag=thekinglegend-21",
+  audible: `https://www.amazon.it/dp/B075CPFY5Y?actionCode=AZIOther35606092201BR&tag=${AMAZON_TAG}`,
 
-  amazonMusic:
-    "https://www.amazon.it/gp/dmusic/promotions/AmazonMusicUnlimited?tag=thekinglegend-21",
+  amazonMusic: `https://www.amazon.it/gp/dmusic/promotions/AmazonMusicUnlimited?tag=${AMAZON_TAG}`,
 } as const;
